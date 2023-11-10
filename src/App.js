be53a2168d32305deec5ba7a54686522db38c017
divyamaggarwal12345 <aggdiv@gmail.com>
2023-11-10 20:18:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LETS TEST OUR DEPLOYMENT ON VERCEL
         </p>
         <a
           className="App-link"
